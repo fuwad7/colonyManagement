c899f4b267d667ef09eb1cd0911605e71f47d9eb
Fuad Hasan <nefarious797@gmail.com>
2026-08-03 15:55:51 +0600

    added Occupancy entity

--- src/main/java/com/example/colonyManagement/entity/Occupancy.java
+++ src/main/java/com/example/colonyManagement/entity/Occupancy.java
@@ -1,4 +1,33 @@
 package com.example.colonyManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "occupancies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Occupancy {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+
+@Enumerated(EnumType.STRING)
+    private OccupancyType OccupancyType;
+@ManyToOne
+    @JoinColumn(name = "flat_id", nullable = false)
+    private Flat flat;
+@ManyToOne
+    @JoinColumn(name = "person_id", nullable = false)
+    private Person person;
+
+@ManyToOne
+    @JoinColumn(name ="rented_from_id")
+    private Person rentedFrom;
+public enum OccupancyType {
+    OWNER, TENANT, SUB_TENANT
+}
+
 }
