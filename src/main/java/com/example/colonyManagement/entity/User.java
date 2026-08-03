b9773baf2e49adf79711f02d2a7d81afaea8b046
Fuad Hasan <nefarious797@gmail.com>
2026-08-03 14:46:34 +0600

    updated user entity

--- src/main/java/com/example/colonyManagement/entity/User.java
+++ src/main/java/com/example/colonyManagement/entity/User.java
@@ -1,4 +1,23 @@
 package com.example.colonyManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role; // ADMIN, USER
 }
