25034afe5a79744c2aa89c6e96016f7bf95a8645
Fuad Hasan <nefarious797@gmail.com>
2026-08-03 15:36:27 +0600

    added person entity

--- src/main/java/com/example/colonyManagement/entity/Person.java
+++ src/main/java/com/example/colonyManagement/entity/Person.java
@@ -1,4 +1,23 @@
 package com.example.colonyManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "persons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String phone;
+    private String cnicOrId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
