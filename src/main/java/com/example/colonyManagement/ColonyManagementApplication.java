d0df857e9404714dc8240a4fd30cbb124d0cb1cb
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-03 11:47:30 +0600

    git init

---
+++ src/main/java/com/example/colonyManagement/ColonyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.colonyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColonyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColonyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/colonyManagement/ColonyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colonyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColonyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
