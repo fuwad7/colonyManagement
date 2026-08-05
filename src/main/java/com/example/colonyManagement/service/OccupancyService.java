5c0f4ad5603ccd25692f288363870079bd213990
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-05 12:33:32 +0600

    updated

--- src/main/java/com/example/colonyManagement/service/OccupancyService.java
+++ src/main/java/com/example/colonyManagement/service/OccupancyService.java
@@ -5,7 +5,19 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class OccupancyService {
     private OccupancyRepository occupancyRepository;
-
+public Occupancy saveOccupancy(Occupancy occupancy) {
+    return occupancyRepository.save(occupancy);
+}
+public List<Occupancy>getAllOccupancy(){
+    return occupancyRepository.findAll();
+}
+public Optional<Occupancy>getOccupancyById(int id){
+    return occupancyRepository.findById(id);
+}
+public void deleteOccupancy(int id){
+ occupancyRepository.deleteById(id);
+}
 }
