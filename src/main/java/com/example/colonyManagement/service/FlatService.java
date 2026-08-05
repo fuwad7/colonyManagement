f28bc010579feffadbe5b21863d6423c50c2c4a6
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-05 12:13:06 +0600

    added buildingService

--- src/main/java/com/example/colonyManagement/service/FlatService.java
+++ src/main/java/com/example/colonyManagement/service/FlatService.java
@@ -2,6 +2,8 @@ package com.example.colonyManagement.service;
 
 import com.example.colonyManagement.entity.Flat;
 import java.util.List;
+import java.util.Optional;
+
 import com.example.colonyManagement.repository.FlatRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 
 public class FlatService {
+private final FlatRepository flatRepository;
 
+public FlatService (FlatRepository flatRepository) {
+    this.flatRepository = flatRepository;
+}
+public List<Flat>getAllFlats(){
+    return flatRepository.findAll();
+}
+public Optional<Flat>getFlatById(Long id) {
+    return flatRepository.findById(id);
+}
+public Flat createOrUpdateFlat(Flat flat){
+    return flatRepository.save(flat);
+}
+public void deleteFlat(Long id){
+    flatRepository.deleteById(id);}
 
 }
