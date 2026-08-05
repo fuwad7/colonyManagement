19d03bf152a71a55eb7c6508d0c3854575bc8992
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-05 16:55:07 +0600

    updated

--- src/main/java/com/example/colonyManagement/service/PersonService.java
+++ src/main/java/com/example/colonyManagement/service/PersonService.java
@@ -1,4 +1,36 @@
 package com.example.colonyManagement.service;
 
+import com.example.colonyManagement.entity.Person;
+import com.example.colonyManagement.repository.PersonRepository;
+import com.example.colonyManagement.service.PersonService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PersonService {
+private  PersonRepository personRepository;
+
+public Person savePerson(Person person){
+    return personRepository.save(person);
+}
+public List<Person>getAllPerson(){
+    return personRepository.findAll();
+}
+public Optional<Person>getPersonById(Long id){
+    return personRepository.findById(id);
+}
+public Person updatePerson(Long id,Person personDetails){
+    return personRepository.findById(id).map(person -> {
+        personDetails.setFullName(personDetails.getFullName());
+        personDetails.setPersonId(personDetails.getPersonId());
+        personDetails.setPhone(personDetails.getPhone());
+        personDetails.setUser(personDetails.getUser());
+        return personRepository.save(person);
+    }).orElseThrow(() -> new RuntimeException("Person not found with id: " + id));
+
+}
+public void deletePerson(Long id) {
+    personRepository.deleteById(id);
+}
 }
