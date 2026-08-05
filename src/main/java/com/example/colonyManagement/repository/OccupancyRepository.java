877626ccab8b5154c6a493479aa0392d31387e79
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-05 13:06:08 +0600

    updated

--- src/main/java/com/example/colonyManagement/repository/OccupancyRepository.java
+++ src/main/java/com/example/colonyManagement/repository/OccupancyRepository.java
@@ -4,7 +4,12 @@ import com.example.colonyManagement.entity.Occupancy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
 public interface OccupancyRepository extends JpaRepository<Occupancy, Integer>{
+    List<Occupancy> findByPersonId(int person_id);
+
+    List<Occupancy> findByOccupancyType(Occupancy.OccupancyType occupancyType);
 }
--- src/main/java/com/example/colonyManagement/service/OccupancyService.java
+++ src/main/java/com/example/colonyManagement/service/OccupancyService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Service
 public class OccupancyService {
     private OccupancyRepository occupancyRepository;
-public Occupancy saveOccupancy(Occupancy occupancy) {
+public Occupancy createOccupancy(Occupancy occupancy) {
     return occupancyRepository.save(occupancy);
 }
 public List<Occupancy>getAllOccupancy(){
@@ -16,6 +16,20 @@ public List<Occupancy>getAllOccupancy(){
 }
 public Optional<Occupancy>getOccupancyById(int id){
     return occupancyRepository.findById(id);
+}
+public List<Occupancy>getOccupancyByPerson(int personId){
+    return occupancyRepository.findByPersonId(personId);
+}
+public List<Occupancy>getOccupancyByType(Occupancy.OccupancyType type){
+    return occupancyRepository.findByOccupancyType(type);
+}
+public Occupancy UpdateOccupancy(int id,Occupancy occupancyDetails){
+    Occupancy existingOccupancy = occupancyRepository.findById(id);
+existingOccupancy.setOccupancyType(occupancyDetails.getOccupancyType());
+existingOccupancy.setFlat(occupancyDetails.getFlat());
+existingOccupancy.setRentedFrom(occupancyDetails.getRentedFrom());
+existingOccupancy.setPerson(occupancyDetails.getPerson());
+return occupancyRepository.save(existingOccupancy);
 }
 public void deleteOccupancy(int id){
  occupancyRepository.deleteById(id);
