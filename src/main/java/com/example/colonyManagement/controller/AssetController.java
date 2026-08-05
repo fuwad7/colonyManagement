361d4bc2b32ee43b3fd1927110c3102ec1f0dfca
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-05 19:25:51 +0600

    updated AssetController

--- src/main/java/com/example/colonyManagement/controller/AssetController.java
+++ src/main/java/com/example/colonyManagement/controller/AssetController.java
@@ -1,14 +1,45 @@
 package com.example.colonyManagement.controller;
 
+import com.example.colonyManagement.entity.Asset;
 import com.example.colonyManagement.service.AssetService;
-import com.example.colonyManagement.repository.AssetRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
 @RestController
 
 @RequestMapping("/api/assets")
 public class AssetController {
 
+private final AssetService assetService;
+
+public AssetController (AssetService assetService){
+    this.assetService = assetService;
+}
+@GetMapping
+public ResponseEntity<List<Asset>>getAllAssets(){
+    List<Asset> assets = assetService.getAllAssets();
+    return ResponseEntity.ok(assets);
+}
+
+@GetMapping("/{id}")
+    public ResponseEntity<Asset>getAssetById(@PathVariable Long id){
+    return assetService.getAssetById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+}
+@PostMapping
+    public ResponseEntity<Asset>saveAsset(@RequestBody Asset asset){
+    Asset savedAsset = assetService.saveAsset(asset);
+    return new ResponseEntity<>(savedAsset, HttpStatus.CREATED);
+}
+@GetMapping("/type")
+public ResponseEntity<List<Asset>>getAssetsByType(@RequestParam Asset.AssetType type){
+List<Asset> assets = assetService.getAssetsByType(type);
+return ResponseEntity.ok(assets);
+}
+@DeleteMapping("/{id}")
+    public ResponseEntity<Void>deleteAsset(@PathVariable Long id){
+    assetService.deleteAsset(id);
+    return ResponseEntity.noContent().build();
+}
 }
