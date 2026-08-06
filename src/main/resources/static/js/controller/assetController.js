da5fd0028a5399185f8216bea32e97db913ecd80
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-06 19:28:11 +0600

    updated

--- src/main/resources/static/js/controller/assetController.js
+++ src/main/resources/static/js/controller/assetController.js
@@ -1,26 +1,129 @@
-(function (){
+(function () {
     'use strict';
     angular
-    .module('colonyManagementApp')
-        .controller('AssetController',)
+        .module('colonyManagementApp')
+        .controller('AssetController', AssetController);
+
     AssetController.$inject = ['AssetService'];
+
     function AssetController(AssetService) {
         var vm = this;
         vm.assets = [];
-        vm.curentAssets = {};
-        vm.selectedType = '';
-        vm.errorMessage = '';
-        vm.isLoading = false;
+        vm.currentAsset = {};
+        vm.searchType = '';
+        vm.error = null;
+        vm.loading = false;
 
-        vm.loadAllAssets = function loadAllAssets;
-        vm.loadAssetsByType = function loadAssetsByType;
-        vm.selectAssetForEdit = function selectAssetForEdit;
-        vm.saveOrUpdateAsset = function saveOrUpdateAsset;
-        vm.deleteAsset = function deleteAsset;
-        vm.resetForm = function resetForm;
+        vm.getAssets = getAssets;
+        vm.getAssetsByType = getAssetsByType;
+        vm.getAssetById = getAssetById;
+        vm.editAsset = editAsset;
+        vm.saveOrUpdate = saveOrUpdate;
+        vm.deleteAsset = deleteAsset;
+        vm.clearForm = clearForm;
 
+        activate();
 
+        function activate() {
+            getAssets();
+        }
 
+        function getAssets() {
+            vm.loading = true;
+            vm.error = null;
 
+            AssetService.getAllAssets()
+                .then(function (data) {
+                    vm.assets = data;
+                })
+                .catch(function (err) {
+                    vm.error = err;
+                })
+                .finally(function () {
+                    vm.loading = false;
+                });
+        }
+
+        function getAssetsByType() {
+            if (!vm.searchType) {
+                return getAssets();
+            }
+
+            vm.loading = true;
+            AssetService.getAssetsByType(vm.searchType)
+                .then(function (data) {
+                    vm.assets = data;
+                })
+                .catch(function (err) {
+                    vm.error = err;
+                })
+                .finally(function () {
+                    vm.loading = false;
+                });
+        }
+
+        function getAssetById(id) {
+            vm.loading = true;
+            vm.error = null;
+
+            AssetService.getAssetById(id)
+                .then(function (data) {
+                    vm.currentAsset = data;
+                })
+                .catch(function (err) {
+                    vm.error = err;
+                })
+                .finally(function () {
+                    vm.loading = false;
+                });
+        }
+
+        function editAsset(asset) {
+            getAssetById(asset.id);
+        }
+
+        function saveOrUpdate(){
+            vm.loading = true;
+            vm.error = null;
+
+            if (vm.currentAsset.id) {
+                AssetService.updateAsset(vm.currentAsset.id, vm.currentAsset)
+                    .then(handleWriteSuccess)
+                    .catch(handleWriteError);
+            } else {
+                AssetService.saveAsset(vm.currentAsset)
+                    .then(handleWriteSuccess)
+                    .catch(handleWriteError);
+            }
+
+            function handleWriteSuccess() {
+                getAssets();
+                clearForm();
+            }
+
+            function handleWriteError(err) {
+                vm.error = err;
+                vm.loading = false;
+            }
+        }
+
+        function deleteAsset(id) {
+            if (!confirm('You want to remove this asset?')) {
+                return;
+            }
+
+            vm.loading = true;
+            AssetService.deleteAsset(id)
+                .then(function () {
+                    getAssets();
+                })
+                .catch(function (err) {
+                    vm.error = err;
+                    vm.loading = false;
+                });
+        }
+        function clearForm() {
+            vm.currentAsset = {};
+        }
     }
-}
+})();
