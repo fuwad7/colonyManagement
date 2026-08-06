9970ea0a79de403f1644e314176d4bb1a8e44c2f
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-06 18:03:10 +0600

    updated

--- src/main/resources/static/js/service/assetAssignment.Service.js
+++ src/main/resources/static/js/service/assetAssignment.Service.js
@@ -16,39 +16,46 @@
             getAssignmentById: getAssignmentById,
             getAssignmentsByPerson: getAssignmentsByPerson,
             getAssignmentsByAsset: getAssignmentsByAsset,
+            updateAssignment: updateAssignment,
             deleteAssignment: deleteAssignment
         };
 
         return service;
 
-        function createBuilding(building) {
-            return $http.post(baseUrl, building)
+        function createAssignment(assetAssignment) {
+            return $http.post(baseUrl, assetAssignment)
                 .then(handleSuccess, handleError);
         }
 
-        function getAllBuildings() {
+        function getAllAssignments() {
             return $http.get(baseUrl)
                 .then(handleSuccess, handleError);
         }
 
-        function getBuildingById(id) {
+        function getAssignmentById(id) {
             return $http.get(baseUrl + '/' + id)
                 .then(handleSuccess, handleError);
         }
-
-        function updateBuilding(id, buildingDetails) {
-            return $http.put(baseUrl + '/' + id, buildingDetails)
+        function getAssignmentsByPerson(personId) {
+            return $http.get(baseUrl + '/person/' + personId)
+                .then(handleSuccess, handleError);
+        }
+        function getAssignmentsByAsset(assetId) {
+            return $http.get(baseUrl + '/asset/' + assetId)
+                .then(handleSuccess, handleError);
+        }
+        function updateAssignment(id, assetAssignmentDetails) {
+            return $http.put(baseUrl + '/' + id, assetAssignmentDetails)
                 .then(handleSuccess, handleError);
         }
 
-        function deleteBuilding(id) {
+        function deleteAssignment(id) {
             return $http.delete(baseUrl + '/' + id)
                 .then(handleSuccess, handleError);
         }
         function handleSuccess(response) {
             return response.data;
         }
-
         function handleError(error) {
             return Promise.reject(error.data || error.message || 'Server Error');
         }
