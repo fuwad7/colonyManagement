f1a0c29176f4242ca9299c0a7bfa53c9f5d31aa2
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-16 13:24:40 +0600

    added messageSource, localeResolver

--- src/main/java/com/example/colonyManagement/config/LocaleConfig.java
+++ src/main/java/com/example/colonyManagement/config/LocaleConfig.java
@@ -3,16 +3,28 @@ package com.example.colonyManagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import java.util.Arrays;
+import java.util.Locale;
 
-
+@Configuration
 public class LocaleConfig {
 
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-
-    messageSource.setDefaultEncoding("UTF-8");
-    messageSource.setBasename("classpath:/messages");
-    return messageSource;}
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:messages");
+        source.setDefaultEncoding("UTF-8");
+        source.setCacheSeconds(3600);
+        return source;
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setSupportedLocales(Arrays.asList(Locale.ENGLISH, new Locale("bn")));
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
 }
