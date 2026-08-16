979cb91bfada841803eebb9e648462bb34a74e31
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-16 13:31:45 +0600

    added LanguageController

---
+++ src/main/java/com/example/colonyManagement/controller/LanguageController.java
@@ -0,0 +1,23 @@
+package com.example.colonyManagement.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+@RequestMapping("/language")
+public class LanguageController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/status")
+    public ResponseEntity<?> getStatusMessage() {
+
+    }
+
+}
