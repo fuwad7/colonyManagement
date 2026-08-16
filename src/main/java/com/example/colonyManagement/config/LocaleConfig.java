79b03f75b6fefccbce5d862ba785c208d72c7a7c
fuwad7 <hasan16-669@s.diu.edu.bd>
2026-08-16 13:01:11 +0600

    language config

---
+++ src/main/java/com/example/colonyManagement/config/LocaleConfig.java
@@ -0,0 +1,18 @@
+package com.example.colonyManagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+
+
+public class LocaleConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+    messageSource.setDefaultEncoding("UTF-8");
+    messageSource.setBasename("classpath:/messages");
+    return messageSource;}
+}
